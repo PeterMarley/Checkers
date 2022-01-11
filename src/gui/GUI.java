7b64ff0b4d01a971a7d619abe22686b370176598
BigJeffTheChef <marleygpeter@gmail.com>
2022-01-11 23:49:32 +0000

    bloody 0,0 - imma get you tomorrow sonny jim

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -25,6 +25,7 @@ public class GUI {
 		for (int row = 0; row < gameBoard.length; row++) {
 			for (int col = 0; col < gameBoard[row].length; col++) {
 				GamePiece tmp = null;
+				SquareLabel tmpSquare = null;
 				if (row < 3 || row > 4) {
 					if ((row + 1) % 2 == 1) {
 						if ((col + 1) % 2 == 0) {
@@ -36,11 +37,19 @@ public class GUI {
 						}
 					}
 				}
-				gameBoard[row][col] = new SquareLabel(Color.BLUE, tmp);
-				gameBoard[row][col].setBounds((col * 100)+100, (row * 100)+100, 100, 100);
+				//TODO get index 0,0 to actually have correct alignment and position (see comment after loop)
+				tmpSquare = new SquareLabel(Color.BLUE, tmp);
+				gameBoard[row][col] = tmpSquare;
+				int x, y;
+				x = (col + 1) * 100;
+				y = (row + 1) * 100;
+				gameBoard[row][col].setBounds(x, y, 100, 100);
 				frame.add(gameBoard[row][col]);
 			}
 		}
+		//SquareLabel tmp = new SquareLabel(Color.BLUE, null);
+		//tmp.setBounds(100, 100, 100, 100);
+		//frame.add(tmp);
 		//SquareLabel testLabl = new SquareLabel(0, 0, Color.BLACK, bk);
 
 		//testLabl.setBounds(0, 0, 100, 100);
