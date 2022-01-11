54fd792200ec9e851e124695f7e3c9377ac02593
BigJeffTheChef <marleygpeter@gmail.com>
2022-01-11 23:31:43 +0000

    a little more work on GUI. Cant get bloody [0,0] to render border. will look when not wrecked. dont want to resort to WYSIWYG editor, need to learn this stuff

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -1,5 +1,52 @@
 package gui;
 
+import java.awt.Color;
+
+import checkers.GamePiece;
+
 public class GUI {
+	public static void main(String[] args) {
+		// Test GamePieces
+		//		GamePiece bp = new GamePiece(0);
+		//		GamePiece bk = new GamePiece(0);
+		//		bk.setToKing();
+		//		GamePiece wp = new GamePiece(1);
+		//		GamePiece wk = new GamePiece(1);
+		//		wk.setToKing();
+
+		// instantiate frame
+		MyJFrame frame = new MyJFrame();
+		// declare and init board
+		SquareLabel[][] gameBoard = new SquareLabel[8][8];
+		// set frame to visible
+		frame.setVisible(true);
+
+		// set all gameBoard square labels
+		for (int row = 0; row < gameBoard.length; row++) {
+			for (int col = 0; col < gameBoard[row].length; col++) {
+				GamePiece tmp = null;
+				if (row < 3 || row > 4) {
+					if ((row + 1) % 2 == 1) {
+						if ((col + 1) % 2 == 0) {
+							tmp = new GamePiece(0);
+						}
+					} else {
+						if ((col + 1) % 2 == 1) {
+							tmp = new GamePiece(0);
+						}
+					}
+				}
+				gameBoard[row][col] = new SquareLabel(Color.BLUE, tmp);
+				gameBoard[row][col].setBounds((col * 100)+100, (row * 100)+100, 100, 100);
+				frame.add(gameBoard[row][col]);
+			}
+		}
+		//SquareLabel testLabl = new SquareLabel(0, 0, Color.BLACK, bk);
+
+		//testLabl.setBounds(0, 0, 100, 100);
+		frame.setSize(1000, 1000);
+		frame.setLayout(null);
+		//frame.add(testLabl);
+	}
 
 }
--- src/gui/SquareLabel.java
+++ src/gui/SquareLabel.java
@@ -9,35 +9,36 @@ import javax.swing.border.Border;
 
 import checkers.GamePiece;
 
-public class ChessSquareLabel extends JLabel {
-	public ChessSquareLabel(int x, int y, Color color, GamePiece p) {
+public class SquareLabel extends JLabel {
+	public SquareLabel(Color color, GamePiece p) {
 		// define border
 		Border border = BorderFactory.createLineBorder(Color.BLACK, 1);
-		
+
 		// define icon
 		ImageIcon icon = null;
-		if (p.isKing()) {
-			if (p.getTeam() == 0) {
-				icon = Enums.Pieces.BLACK_KING.get();
+		if (p != null) {
+			if (p.isKing()) {
+				if (p.getTeam() == 0) {
+					icon = Enums.Pieces.BLACK_KING.get();
+				} else {
+					icon = Enums.Pieces.WHITE_KING.get();
+				}
 			} else {
-				icon = Enums.Pieces.WHITE_KING.get();
+				if (p.getTeam() == 0) {
+					icon = Enums.Pieces.BLACK_PIECE.get();
+				} else {
+					icon = Enums.Pieces.WHITE_PIECE.get();
+				}
 			}
-		} else {
-			if (p.getTeam() == 0) {
-				icon = Enums.Pieces.BLACK_PIECE.get();
-			} else {
-				icon = Enums.Pieces.WHITE_PIECE.get();
-			}
+			this.setIcon(icon);
 		}
 		
 		// define properties
-		this.setIcon(icon);
 		this.setBackground(color);
 		this.setBorder(border);
 		this.setOpaque(false);
 		this.setHorizontalAlignment(JLabel.CENTER);
 		this.setVerticalAlignment(JLabel.CENTER);
-		//this.setBounds(x, y, 100, 100);
 
 	}
 
