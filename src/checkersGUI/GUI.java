115c2391163b5f6595681b84c27ec7573790a032
BigJeffTheChef <marleygpeter@gmail.com>
2022-01-14 03:17:11 +0000

    basic intro screen implemented. Man Swing is awesome!

--- src/checkersGUI/GUI.java
+++ src/checkersGUI/GUI.java
@@ -6,8 +6,11 @@ import checkersGUI.abstracts.GUIFrame;
 
 /**
  * GUI for my 2 player checkers program
- * @Attributions Images: https://woodexpressions.com/wp-content/uploads/2016/07/258015Main2DB.jpg<br>
- *               Images edited by me to give them the correct size and background transparency on GIMP
+ * @Attributions - Checkers Pieces Images*:<br>https://woodexpressions.com/wp-content/uploads/2016/07/258015Main2DB.jpg<br><br>
+ *               - Checker Board Intro Image:<br>https://content.mycutegraphics.com/graphics/play/checker-game-clip-art.png<br>
+ *               <br>
+ *               * Images edited by me to give them the correct size and background transparency on GIMP
+ * 
  * @author Peter Marley
  * @StudentNum 13404067
  * @email pmarley03@qub.ac.uk
@@ -16,6 +19,9 @@ import checkersGUI.abstracts.GUIFrame;
  */
 public class GUI {
 
+	public static GUIFrame frameIntro = new GUIFrameIntro();
+	public static GUIFrame frameMain = new GUIFrameMain();
+	
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
@@ -25,13 +31,7 @@ public class GUI {
 	}
 
 	public static void initGUI() {
-
-		GUIFrame frameMain = new GUIFrameMain();
-		GUIFrame frameIntro = new GUIFrameIntro();
-
-		frameMain.setVisible(true);
-		frameIntro.setVisible(false);
-
+		frameIntro.setVisible(true);
 	}
 
 }
--- src/checkersGUI/GUIFrameIntro.java
+++ src/checkersGUI/GUIFrameIntro.java
@@ -1,11 +1,53 @@
 package checkersGUI;
 
+import java.awt.Color;
+import java.awt.GridLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import checkersGUI.Enums.Length;
 import checkersGUI.abstracts.GUIFrame;
 
 public class GUIFrameIntro extends GUIFrame {
 
 	public GUIFrameIntro() {
 		super();
+		this.setLayout(new GridLayout(2, 1, 0, 10));
+		JLabel checkerboard = new JLabel();
+		checkerboard.setIcon(new ImageIcon("./images/checkerboard.png"));
+		checkerboard.setVerticalAlignment(JLabel.CENTER);
+		checkerboard.setHorizontalAlignment(JLabel.CENTER);
+		
+		JPanel textPanel = new JPanel();
+		textPanel.setLayout(new GridLayout(2, 1));
+		textPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK, Length.SQUARE_BORDER_THICKNESS.value()));
+		textPanel.setBackground(Color.LIGHT_GRAY);
+		textPanel.setForeground(Color.WHITE);
+		
+		JLabel textLabel = new JLabel();
+		String welcome = String.format("Welcome to Checkers!");
+		textLabel.setVerticalAlignment(JLabel.CENTER);
+		textLabel.setHorizontalAlignment(JLabel.CENTER);
+		textLabel.setText(welcome);
+		
+		JButton startButton = new JButton();
+		startButton.setText("Start the game!");
+		startButton.addActionListener(e -> startGame());
+		
+		//combine components
+		textPanel.add(textLabel);
+		textPanel.add(startButton);
+		this.add(checkerboard);
+		this.add(textPanel);
+	}
+	
+	public static void startGame() {
+		GUI.frameMain.setVisible(true);
+		GUI.frameIntro.setVisible(false);
 	}
 
 }
